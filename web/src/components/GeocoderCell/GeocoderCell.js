88464a10dc661df931440a8802c4c611f4a890e0
Do Huynh <huynhdoo@gmail.com>
2022-04-11 22:34:49 +0200

    Calculate distance from HQ

--- web/src/components/GeocoderCell/GeocoderCell.js
+++ web/src/components/GeocoderCell/GeocoderCell.js
@@ -1,3 +1,5 @@
+import * as turf from '@turf/turf'
+
 export const QUERY = gql`
   query GetGeocoderQuery($zip: String!) {
     geocoder: getLocation(zip: $zip) {
@@ -18,13 +20,21 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ geocoder }) => {
+  var from = turf.point([-0.580036, 44.841225]);
+  var to = turf.point([geocoder.longitude, geocoder.latitude]);
+  var options = {units: 'kilometers'};
+  var distance = turf.distance(from, to, options);
+
   return (
     <section>
         <h1>{geocoder.address}</h1>
         <h2>
-          <span>
-            {geocoder.longitude}, {geocoder.latitude}
-          </span>
+          <p>
+            [{geocoder.longitude}, {geocoder.latitude}]
+          </p>
+          <p>
+            {Math.round(distance)} km from HQ (65 quai de Brazza 33000 Bordeaux)
+          </p>
         </h2>
       </section>
   )
