49114e571d5d137f40130520143f0d3a953651d0
Do Huynh <huynhdoo@MacBook-Air-de-Do.local>
2022-04-29 17:30:20 +0200

    update geocoder proximity

--- api/src/services/mapbox/mapbox.js
+++ api/src/services/mapbox/mapbox.js
@@ -5,7 +5,7 @@ export const location = async ({ query }) => {
   // console.log( `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.580036%2C44.841225&types=postcode&access_token=${MAPBOX_TOKEN}`)
   const response = await fetch(
   //  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.580036%2C44.841225&access_token=${MAPBOX_TOKEN}`
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=ip&types=place%2Cpostcode%2Caddress&access_token=${MAPBOX_TOKEN}`
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.580036%2C44.841225&types=address&language=fr&access_token=${MAPBOX_TOKEN}`
   )
   const json = await response.json()
 
@@ -22,7 +22,7 @@ export const locations = async ({ query }) => {
   // console.log( `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.580036%2C44.841225&types=postcode&access_token=${MAPBOX_TOKEN}`)
   const response = await fetch(
   //  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.580036%2C44.841225&access_token=${MAPBOX_TOKEN}`
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=fr&proximity=ip&types=address&language=fr&access_token=${MAPBOX_TOKEN}`
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=fr&proximity=-0.580036%2C44.841225&types=address&language=fr&access_token=${MAPBOX_TOKEN}`
   )
   const json = await response.json()
 
