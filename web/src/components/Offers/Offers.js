72ee0d5a450cc3d925e6e92d41f147f18ddd86d8
Do Huynh <huynhdoo@MacBook-Air-de-Do.local>
2022-04-29 20:31:02 +0200

    Add subscription form

--- web/src/components/Offers/Offers.js
+++ web/src/components/Offers/Offers.js
@@ -35,69 +35,60 @@ const Offers = ({meals, onChange}) => {
   }
 
   return (
-    <div className="md:flex md:flex-row pt-4">
-      <div className="md:col-span-1 md:basis-1/3">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">Estimation</h3>
-        <p className="text-sm text-gray-600">
-          <Estimate meals={meals} />
-        </p>
-      </div>
-      <div className="md:pl-4 w-full md:basis-2/3">
-        <RadioGroup value={service} onChange={(e) => {onChange(e); setService(e)}} className="space-y-2">
-          <RadioGroup.Label className="text-lg font-medium leading-6 text-gray-900">Les offres</RadioGroup.Label>
-          {
-            offers.map((offer) => (
-              <>
-              {offer.qty > 0 ?
-                <RadioGroup.Option value={offer.name} className={({ active, checked }) =>
-                  `${
-                    active
-                      ? 'ring-2 ring-offset-2 ring-offset-green-500 ring-white ring-opacity-60'
-                      : ''
-                  }
-                  ${
-                    checked ? 'bg-green-900 bg-opacity-75 text-white' : 'bg-white'
-                  }
-                    relative rounded-lg shadow-md px-5 py-4 cursor-pointer flex focus:outline-none`
-                }>
-                {({ active, checked }) => (
-                  <>
-                    <div className="flex items-center justify-between w-full">
-                      <div className="flex items-center">
-                        <div className="text-sm">
-                          <RadioGroup.Label
-                            as="p"
-                            className={`font-medium  ${
-                              checked ? 'text-white' : 'text-gray-900'
-                            }`}
-                          >
-                          {offer.qty}x {offer.name} {offer.liter}L
-                          </RadioGroup.Label>
-                          <RadioGroup.Description
-                            as="span"
-                            className={`inline ${
-                              checked ? 'text-green-100' : 'text-gray-500'
-                            }`}
-                          >
-                          <span>{(offer.qty*offer.liter*PRICE_BY_LITER).toFixed(2)}€ par semaine</span>
-                          </RadioGroup.Description>
-                        </div>
+    <>
+      <RadioGroup value={service} onChange={(e) => {onChange(e); setService(e)}} className="space-y-2">
+        {
+          offers.map((offer) => (
+            <>
+            {offer.qty > 0 ?
+              <RadioGroup.Option value={offer.qty + 'x ' + offer.name + ' ' + offer.liter +'L - ' + (offer.qty*offer.liter*PRICE_BY_LITER).toFixed(2) + '€ par semaine'} className={({ active, checked }) =>
+                `${
+                  active
+                    ? 'ring-2 ring-offset-2 ring-offset-orange-300 ring-white ring-opacity-60'
+                    : ''
+                }
+                ${
+                  checked ? 'bg-orange-600 text-white' : 'bg-white'
+                }
+                  relative rounded-lg shadow-md px-5 py-4 cursor-pointer flex focus:outline-none`
+              }>
+              {({ active, checked }) => (
+                <>
+                  <div className="flex items-center justify-between w-full">
+                    <div className="flex items-center">
+                      <div className="text-sm">
+                        <RadioGroup.Label
+                          as="p"
+                          className={`font-medium  ${
+                            checked ? 'text-white' : 'text-gray-900'
+                          }`}
+                        >
+                        {offer.qty}x {offer.name} {offer.liter}L
+                        </RadioGroup.Label>
+                        <RadioGroup.Description
+                          as="span"
+                          className={`inline ${
+                            checked ? 'text-green-100' : 'text-gray-500'
+                          }`}
+                        >
+                        <span>{(offer.qty*offer.liter*PRICE_BY_LITER).toFixed(2)}€ par semaine</span>
+                        </RadioGroup.Description>
                       </div>
-                      {checked && (
-                        <div className="flex-shrink-0 text-white">
-                          <CheckIcon className="w-6 h-6" />
-                        </div>
-                      )}
                     </div>
-                  </>
-                )}
-              </RadioGroup.Option>
-              : ''}
-            </>
-          ))}
-        </RadioGroup>
-      </div>
-    </div>
+                    {checked && (
+                      <div className="flex-shrink-0 text-white">
+                        <CheckIcon className="w-6 h-6" />
+                      </div>
+                    )}
+                  </div>
+                </>
+              )}
+            </RadioGroup.Option>
+            : ''}
+          </>
+        ))}
+      </RadioGroup>
+    </>
   )
 }
 
--- web/src/pages/SearchPage/SearchPage.js
+++ web/src/pages/SearchPage/SearchPage.js
@@ -1,7 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
 import { useState } from 'react'
 import { MetaTags } from '@redwoodjs/web'
-import { Form, Label, TextField, NumberField, Submit } from '@redwoodjs/forms'
+import { Form, Label, TextField, NumberField, EmailField, TelField, DateField, Submit } from '@redwoodjs/forms'
 import LocationField from 'src/components/Location/LocationField'
 import GeocoderCell from 'src/components/Location/GeocoderCell'
 import Offers from 'src/components/Offers'
@@ -9,71 +9,156 @@ import Offers from 'src/components/Offers'
 const SearchPage = () => {
   const [location, setLocation] = useState()
   const [meals, setMeals] = useState()
-  const [offer, setOffer] = useState()
+  const [service, setService] = useState()
 
   const logger = (log) => {
     console.log(log)
   }
 
-  const formSubmit = (data) => {
+  const searchSubmit = (data) => {
     console.log(JSON.stringify(data))
     // setLocation(selected)
     // setMeals(data.meals)
   }
 
-  const commandSubmit = () => {
-    var command = {
-      location : location,
-      meals : meals,
-      offer : offer,
+  const [subscription, setSubscription] = useState()
+  const subscriptionSubmit = (data) => {
+    var sub = {
+      user : data,
+      offer : {
+        location : location,
+        meals : meals,
+        service : service,
+      }
     }
-    console.log(JSON.stringify(command))
+    console.log(JSON.stringify(sub))
+    setSubscription(sub)
   }
 
   return (
   <>
-    <MetaTags title="Recherche" description="Trouver la meilleure solution pour vos déchets organiques" />
+    <MetaTags title="Recherche" description="Trouvez la meilleure solution de tri des déchets organiques" />
     <div>
-      <div className="font-medium text-center sm:text-2xl md:text-3xl mt-16">
-        Trouvez la meilleure solution pour composter vos déchets organiques
+      <div className="font-medium text-center text-2xl md:text-3xl mt-16">
+        Trouvez la meilleure solution de tri des déchets organiques
       </div>
-      <Form onSubmit={formSubmit} className="container mx-auto max-w-xl font-sans text-center">
-        <div className="bg-white rounded-t-lg shadow-lg p-8 mt-8">
-          <Label className="font-medium">
-            Adresse à collecter
-          </Label>
-          <div className="w-full">
-            <LocationField onChange={setLocation} className="text-center w-full rounded-md bg-gray-200 py-2 pl-3 pr-10 text-sm outline-green-800 leading-5 text-gray-900 focus:ring-0"/>
-          </div>
-          <div className="font-medium mt-6">
-            Repas par semaine
+      <div className="container mx-auto max-w-xl font-sans">
+        <Form onSubmit={searchSubmit}>
+          <div className="bg-white rounded-t-lg shadow-lg p-8 mt-8 text-center">
+            <Label className="font-medium block">
+              Adresse à collecter
+            </Label>
+            <LocationField onChange={setLocation} className="block text-center w-full rounded-md bg-gray-200 py-2 pl-3 pr-10 text-sm outline-green-800 leading-5 text-gray-900 focus:ring-0"/>
+            <Label className="font-medium mt-6 block">
+              Repas par semaine
+            </Label>
+            <NumberField onChange={(e) => setMeals(e.target.value)} name="meals" className="block w-full text-center bg-gray-200 rounded-md p-2 text-sm outline-green-800" />
           </div>
           <div>
-            <NumberField onChange={(e) => setMeals(e.target.value)} name="meals" className="text-center w-1/4 bg-gray-200 rounded-md p-2 text-sm outline-green-800" />
+              <Submit
+                disabled={location == null || meals == null}
+                className="sm:text-sm md:text-lg uppercase font-bold bg-green-800 rounded-b-md p-4 text-white w-full shadow-lg">Chercher une solution locale</Submit>
+          </div>
+        </Form>
+        {location &&
+          <div className="text-center bg-white rounded-lg shadow-lg p-4 mt-8 text-lg">
+            <GeocoderCell query={location}/>
+          </div>
+        }
+      </div>
+    </div>
+    { location && meals &&
+      <div>
+        <div className="font-medium text-center text-2xl md:text-3xl mt-16">
+          Choisissez la solution de tri qui vous ressemble
+        </div>
+        <div className="container mx-auto max-w-3xl font-sans">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+            <div>
+              <div className="mt-8 text-lg">
+                <Offers meals={meals} onChange={setService}/>
+              </div>
+            </div>
+            <div>
+              <Form onSubmit={subscriptionSubmit} className="container mx-auto font-sans">
+                <div className="bg-white rounded-t-lg shadow-lg p-8 mt-8">
+                  <Label className="font-medium block">
+                    Prénom
+                  </Label>
+                  <TextField name="firstname" className="capitalize block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    Nom
+                  </Label>
+                  <TextField name="lastname" className="uppercase block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    Société
+                  </Label>
+                  <TextField name="company" className="block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    Mél
+                  </Label>
+                  <EmailField name="email" className="block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    Téléphone
+                  </Label>
+                  <TelField name="phone" className="block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                </div>
+                <div>
+                    <Submit
+                      className="sm:text-sm md:text-lg uppercase font-bold bg-orange-600 rounded-b-md p-4 text-white w-full shadow-lg">Essayer gratuitement</Submit>
+                </div>
+              </Form>
+            </div>
           </div>
         </div>
-        <div>
-            <Submit
-              disabled={location == null || meals == null}
-              className="sm:text-sm md:text-lg uppercase font-bold bg-green-800 rounded-b-md p-4 text-white w-full shadow-lg">Chercher une solution locale</Submit>
+      </div>
+    }
+    { subscription &&
+      <div>
+        <div className="font-medium text-center text-2xl md:text-3xl mt-16">
+          Validez votre contrat de collecte
         </div>
-      </Form>
-    </div>
-    <div className="container mx-auto max-w-xl font-sans">
-      {location &&
-        <div className="text-center mt-8 bg-white rounded-lg shadow-lg p-4 mt-8 text-lg">
-          <GeocoderCell query={location}/>
+        <div className="container mx-auto max-w-6xl font-sans">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+            <div>
+              <div className="bg-white rounded-md shadow-lg p-8 mt-8">
+                <h1 className="uppercase font-bold text-lg text-center mb-6">Contrat de tri des biodéchets</h1>
+                <ul>
+                  <li><span className="font-bold">Société : </span><span className="uppercase">{subscription.user.company}</span></li>
+                  <li><span className="font-bold">Contact : </span><span className="capitalize">{subscription.user.firstname}</span> <span className="uppercase">{subscription.user.lastname}</span></li>
+                  <li><span className="font-bold">Tél : </span><span className="">{subscription.user.phone}</span></li>
+                  <li><span className="font-bold">Mél : </span><span className="">{subscription.user.email}</span></li>
+                  <li><span className="font-bold">Adresse de collecte : </span><span className="">{subscription.offer.location}</span></li>
+                  <li><span className="font-bold">Prestation : </span><span className="lowercase">Collecte et compostage de {subscription.offer.service}</span></li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <Form onSubmit={subscriptionSubmit} className="container mx-auto font-sans">
+                <div className="bg-white rounded-t-lg shadow-lg p-8 mt-8">
+                  <Label className="font-medium block">
+                    Date de démarrage
+                  </Label>
+                  <DateField name="startdate" className="capitalize block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    Numéro carte bleu
+                  </Label>
+                  <TextField name="card_number" className="capitalize block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                  <Label className="font-medium block">
+                    IBAN
+                  </Label>
+                  <TextField name="iban" className="capitalize block w-full bg-gray-200 rounded-md p-2 text-sm outline-orange-300"/>
+                </div>
+                <div>
+                    <Submit
+                      className="sm:text-sm md:text-lg uppercase font-bold bg-blue-600 rounded-b-md p-4 text-white w-full shadow-lg">S'abonner</Submit>
+                </div>
+              </Form>
+            </div>
+          </div>
         </div>
-      }
-      {meals &&
-        <div className="mt-8 mt-8 text-lg">
-          <Offers meals={meals} onChange={setOffer}/>
-        </div>
-      }
-      {location && meals && offer &&
-        <Submit onClick={commandSubmit} className="mt-16 sm:text-sm md:text-lg uppercase font-bold bg-orange-600 rounded-md p-4 text-white w-full shadow-lg">Adhérer</Submit>
-      }
-    </div>
+      </div>
+    }
   </>
   )
 }
