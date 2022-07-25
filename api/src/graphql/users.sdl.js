c50b67d5b56b5c81da0d346326b17b9aa82ff739
Do HUYNH <admin@les-detritivores.co>
2022-07-25 17:35:58 +0200

    remove requireAuth on delete user operations

--- api/src/graphql/users.sdl.js
+++ api/src/graphql/users.sdl.js
@@ -21,6 +21,6 @@ export const schema = gql`
   type Mutation {
     createUser(input: CreateUserInput!): User! @requireAuth
     updateUser(id: Int!, input: UpdateUserInput!): User! @requireAuth
-    deleteUser(id: Int!): User! @requireAuth
+    deleteUser(id: Int!): User! @skipAuth
   }
 `
